f66bec9a5f4fce6d1f8adf72191b592fa5c1632f
Manny Serrano <manny4285@gmail.com>
2025-10-14 00:25:41 -0700

    added index.js

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
-
+import { navLinks } from '../constants'
 const NavBar = () => {
   return (
    <header>
     <nav> 
         <img src ="/LUL_Logo_Horizontal_Brown.png" alt= "LUL Logo" className="h-6 w-auto md:h-15" />
         <ul>
-            {[
-                {label: "Home" }, 
-                {label: "History" }, 
-                {label: "Rho Chapter" },
-                {label: "Join" },
-                {label: "Events"},
-                {label: "Contact"}
-
-            ].map(({ label }) => (
+            {navLinks.map(({ label }) => (
                 <li key = {label}> 
                     <a href = {label} > {label} </a>
                 </li>
             ))}
         </ul>
+
+        <div className = "flex-center gap-3">
+            <button> 
+                <img src = "/search.svg" alt = "Search"/>
+            </button>
+            <button> 
+                <img src = "/cart.svg" alt = "Cart"/>
+            </button>
+        </div>
+
     </nav>
    </header>
   )
---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const navLinks = [
+    {label: "Home" }, 
+    {label: "History" }, 
+    {label: "Rho Chapter" },
+    {label: "Join" },
+    {label: "Events"},
+    {label: "Contact"}
+
+]; 
