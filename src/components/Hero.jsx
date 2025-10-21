61e144ac4acf97e5eaa03e52f983242837647345
Manny Serrano <manny4285@gmail.com>
2025-10-20 23:12:37 -0400

    Implement 3D product viewer section, will change for LUL

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { useEffect, useRef } from "react"
 const Hero = () => {
     const videoRef = useRef(); 
     useEffect(() => {
-        if(videoRef.current) videoRef.current.playbackRate = 1.5; 
+        if(videoRef.current) videoRef.current.playbackRate = 1.0; 
     }, []); 
   return (
     <section id= "hero">
--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,16 +3,22 @@ import clsx from "clsx";
 import {Canvas} from "@react-three/fiber"; 
 import {Box} from "@react-three/drei";
 import { OrbitControls } from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14";
+import StudioLights from "./three/StudioLights.jsx";
+import ModelSwitcher from './three/ModelSwitcher.jsx'; 
+import { useMediaQuery } from "react-responsive";
+
 
 const ProductViewer = () => {
     const { color, scale, setColor, setScale } = useMacbookStore(); 
+    const isMobile = useMediaQuery ({query: '(max-width: 1024px)'}); // checks width whether mobile or tablet
   return (
     <section id ="product-viewer">
 
         <h2> Take a closer look.</h2>
 
         <div className="controls">
-            <p className="info"> Macbook Pro {scale} in {color}</p>
+            <p className="info"> Macbook Pro | Available in 14" & 16" in Space Gray & Dark colors</p>
             <div className="flex-center gap-5 mt-5">
                 <div className ="color-control">
                     <div onClick={() => setColor('#adb5bd')}
@@ -39,8 +45,11 @@ const ProductViewer = () => {
         </div>
 
         <Canvas id="canvas" camera = {{position: [0, 2, 5], fov:50, near: 0.1, far:100}}>
-            <Box position={[0,0,0]} scale ={10*scale} material-color={color} />
-            <OrbitControls enableZoom ={false}/>
+            <StudioLights/>
+            
+            
+
+           <ModelSwitcher scale ={isMobile ? scale - 0.03 : scale} isMobile={isMobile} /> 
 
 
         </Canvas>
