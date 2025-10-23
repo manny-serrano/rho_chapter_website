5ff5645e0d10cf61d3976b9d32f7fd229d929c72
Manny Serrano <manny4285@gmail.com>
2025-10-23 00:47:16 -0400

    add 3 new component files

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import ProductViewer from "./components/ProductViewer";
 import gsap from 'gsap'; 
 import {ScrollTrigger, SplitText} from "gsap/all"; 
 import Showcase from "./components/Showcase";
+import Features from "./components/Features";
+import Highlights from "./components/Highlights";
 
 gsap.registerPlugin(ScrollTrigger) 
 
@@ -16,6 +18,9 @@ const App = () => {
         <Hero />
         <ProductViewer />
         <Showcase /> 
+        <Performance />
+        <Features />
+        <Highlights /> 
 
         
     </main>
