043855305f0e12d622e347dafb85f6a84f2746a0
Manny Serrano <manny4285@gmail.com>
2025-10-26 01:41:58 -0400

    implement footer, will finalize later

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,32 @@
+import { footerLinks } from "../constants/index.js"; 
+
+
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+
+      <div className ="info"> 
+      <p>
+        More ways to get involved, blah blah blah, filler text: jjhfhdfh dhdh. 
+      </p>
+      <img src ="/LUL_Logo_Horizontal_Brown.png" alt= "LUL Logo" className="h-6 w-auto md:h-15" />
+      </div>
+      
+      <hr /> 
+
+      <div className ="links"> 
+        <p> © 2025 La Unidad Latina, Lambda Upsilon Lambda Fraternity Inc., All Rights Reserved. </p>
+
+        <ul>
+          {footerLinks.map(({label, link}) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   )
 }
 
